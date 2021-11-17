7d234f2d90b87c089bf0cb771cafa143dd842cdf
juanantoniobellido <jabellido1@gmail.com>
2021-11-17 09:40:13 +0100

    initiall commit

---
+++ mag01_mlp.py
@@ -0,0 +1,85 @@
+import pickle
+import pandas as pd
+import numpy as np
+import StatsFunctions as stats
+from tensorflow import keras
+from sklearn.preprocessing import StandardScaler
+
+class mag01_mlp():
+    """
+    This class represents the best model/configuration from Loja (RIA station) - aridity index = 0.3162.
+    
+    It uses a SVM model and the following input configuration (being rs the predicted value):
+        ['tx', 'tn', 'ra', 'energyt', 'vpd', 'tdew', 'et0']
+
+    """
+    def __init__(self):
+        # import model
+        filename = "savedModels/MLP_Malaga.h5"
+        self.model = keras.models.load_model(filename)
+
+        # define required inputs
+        self.parameters = ['tx', 'tn', 'ra', 'es', 'et0']
+
+    def import_dataset(self, fileLocation, fileType):
+        """
+        This function import a dataset and convert it into a pandas Dataframe
+        Inputs:
+            fileLocation: "data/dataSet.csv"
+            fileType: string with csv, excel or txt
+        """
+        if fileType == 'csv' or fileType == 'txt':
+            self.dfData = pd.read_csv(fileLocation)
+        elif fileType == 'excel':
+            self.dfData = pd.read_excel(fileLocation)
+        else:
+            AssertionError('this fileType does not exit, use excel, csv and txt instead')
+
+        # filter nan values
+        self.dfData.dropna(inplace=True)
+        self.dfData.reset_index(drop=True, inplace=True)
+        # filter the parameters
+        self.dfData = self.dfData.filter(self.parameters)
+
+    def getStandardDataTest(self):
+        """
+        Split data to train and test
+        """
+        # from training original dataset
+        mean_list = [23.85, 12.54, 29.18, 2.34]
+        std_list = [41.11, 30.84, 88.34, 0.74]
+
+        # we have the input data as x, and the output as y
+        self.x_test = np.array(self.dfData.iloc[:, :-1])
+        self.y_test = np.array(self.dfData.iloc[:, -1])
+
+        # standarization
+        scaler = StandardScaler()
+        scaler.mean_ = mean_list
+        scaler.scale_ = std_list
+
+        # x_train and x_test
+        self.x_test = np.array(scaler.transform(self.x_test))
+
+        return self.x_test, self.y_test
+
+    def predictValues(self):
+        self.y_pred = np.ravel(np.array(self.model.predict(self.x_test)))
+        return self.y_pred
+
+    def statAnalysis(self):
+        rmse = stats.get_root_mean_square_error(self.y_test, self.y_pred)
+        rrmse = stats.get_root_mean_square_error(self.y_test, self.y_pred) / np.mean(self.y_test)
+        mbe = stats.get_mean_bias_error(self.y_test, self.y_pred)
+        r2 = stats.get_coefficient_of_determination(self.y_test, self.y_pred)
+        nse = stats.get_nash_suteliffe_efficiency(self.y_test, self.y_pred)
+
+        return rmse, rrmse, mbe, r2, nse
+
+if __name__ == '__main__':
+    mlModel = mag01_mlp()
+    mlModel.import_dataset("data/MAG01.csv", 'csv')
+    mlModel.getStandardDataTest()
+    mlModel.predictValues()
+    rmse, rrmse, mbe, r2, nse = mlModel.statAnalysis()
+    print(rmse, rrmse, mbe, r2, nse)
